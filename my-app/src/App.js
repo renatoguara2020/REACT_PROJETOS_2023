fcd8f01f5d2c3f2446b569389a1d402a4c693e27
Renato Alves Soares <renatoguara2020@gmail.com>
2023-02-15 12:06:47 -0300

    </div>

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,11 @@
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      <div className="container">
+    
+
+    
         <h1>Hello World React 2023!!!</h1>
-      </div>
-      </header>
-    </div>
+      
   );
 }
 
