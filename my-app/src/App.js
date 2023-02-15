fa015c75aaf27cee9347a2e4f7919ad1c1b64d69
Renato Alves Soares <renatoguara2020@gmail.com>
2023-02-15 12:07:12 -0300

    div

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,14 @@
+import FirstComponent from "./components/FirstComponent";
+
 
 function App() {
   return (
     
 
-    
+  <div>
         <h1>Hello World React 2023!!!</h1>
-      
+        <FirstComponent />
+  </div>    
   );
 }
 
---
+++ my-app/src/components/FirstComponent.js
@@ -0,0 +1,13 @@
+
+// Anatomia de uma função em React JS
+ const  FirstComponent = () => {
+ 
+
+    return(
+        <div>
+           <h1>Primeiro Componente em React 2023 !!!!!!!!!!1</h1>
+        </div>
+    );
+}
+
+export default FirstComponent;
